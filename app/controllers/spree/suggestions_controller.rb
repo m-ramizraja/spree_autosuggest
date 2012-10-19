8aabc6517198651125d391c8920faf207efac137
Calvin Tee <isieox+github@gmail.com>
2012-10-20 02:07:53 +0800

    prevent caching

--- app/controllers/spree/suggestions_controller.rb
+++ app/controllers/spree/suggestions_controller.rb
@@ -1,6 +1,9 @@
 class Spree::SuggestionsController < Spree::BaseController
 
   def index
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
     suggestions = Spree::Suggestion.relevant(params['term'])
 
     if request.xhr?
