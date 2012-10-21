81217e3ced0a91aac30c4f4d806aaa73996c3b11
Jindong Yang <yangjindong@gmail.com>
2012-10-21 16:46:57 +0800

    add admin panel to manage keywords

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Spree::Core::Engine.routes.draw do
   match 'suggestions', :to => 'suggestions#index'
+  namespace :admin do
+  	resources :suggestions
+  end
 end
--- lib/spree/suggestion_configuration.rb
+++ lib/spree/suggestion_configuration.rb
@@ -1,5 +1,5 @@
 class Spree::SuggestionConfiguration < Spree::Preferences::Configuration
-  preference :rows_to_display,    :integer, :default => 5
+  preference :rows_to_display,    :integer, :default => 10
   preference :rows_from_db,       :integer, :default => 45
   preference :count_weight,       :integer, :default => 2
   preference :items_found_weight, :integer, :default => 1
